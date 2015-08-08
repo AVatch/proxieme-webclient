0f6dd0e3257124eb78cad1ef3b9fdc36af86053b
AVatch <avatchinsky@outlook.com>
2015-08-08 14:38:46 -0400

    proxies loading up

--- app/proxies.controllers.js
+++ app/proxies.controllers.js
@@ -5,8 +5,25 @@
 */
 angular.module('proxies.controllers', [])
 
-.controller('ProxiesController', ['$scope',
-  function($scope){
+.controller('ProxiesController', ['$scope', 'Proxie',
+  function($scope, Proxie){
+
+    $scope.proxie = {"proxie":"", "description": ""};
+
+    $scope.proxies = [];
+    var syncProxies = function(){
+      Proxie.getProxies()
+        .then(function(s){
+          if(s.status==200){ return s.data; }
+          else{ throw "error getting proxies"; }
+        }, function(e){console.log(e);})
+
+        .then(function(proxies){
+          console.log(proxies)
+          $scope.proxies = proxies.results;
+
+        }, function(e){console.log(e);});
+    }; syncProxies();
   
 }])
 
--- app/proxies.services.js
+++ app/proxies.services.js
@@ -4,3 +4,38 @@
 * Description
 */
 angular.module('proxies.services', [])
+
+.factory('Proxie', ['$http', 'Authentication', 'VERSION', 'DOMAIN',
+  function($http, Authentication, VERSION, DOMAIN){
+  
+    var getProxies = function(){
+      var token = Authentication.getToken();
+      var response = $http({
+                        url: DOMAIN + '/api/' + VERSION + '/proxies/',
+                        method: 'GET',
+                        headers: { 
+                          'Content-Type': 'application/json',
+                          'Authorization': 'Token ' + token },
+                        data: ''
+                      });
+      return response;
+    };
+
+    var postProxie = function(proxie){
+      var token = Authentication.getToken();
+      var response = $http({
+                        url: DOMAIN + '/api/' + VERSION + '/proxies/',
+                        method: 'POST',
+                        headers: { 
+                          'Content-Type': 'application/json',
+                          'Authorization': 'Token ' + token },
+                        data: proxie
+                      });
+      return response;
+    };
+
+    return{
+      getProxies: getProxies,
+      postProxie: postProxie
+    };
+}])
