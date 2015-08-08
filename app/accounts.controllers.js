af954f26fe1f31cfe75be661e7944f6fef824e25
AVatch <avatchinsky@outlook.com>
2015-08-08 17:31:06 -0400

    profile page renders account

--- app/accounts.controllers.js
+++ app/accounts.controllers.js
@@ -5,7 +5,23 @@
 */
 angular.module('accounts.controllers', [])
 
-.controller('AccountController', ['$scope',
- function($scope){
+.controller('AccountController', ['$scope', '$state', 'Account',
+ function($scope, $state, Account){
   
+  var pk = $state.params.pk;
+
+  $scope.account = {};
+  var syncAccount = function(){
+    Account.getAccount(pk)
+      .then(function(s){
+        if(s.status==200){ return s.data; }
+        else{ throw "error getting account"; }
+      }, function(e){console.log(e);})
+
+      .then(function(account){
+        $scope.account = account;
+      }, function(e){console.log(e);});
+
+  }; syncAccount();
+
 }])
